3fd0f406752f9884284fda94dbaf0b4a3bb283b1
“VICKIYZ” <yzha0265@uni.sydney.edu.au>
2023-10-28 15:11:18 +1100

    updated one set of leave

--- sketch.js
+++ sketch.js
@@ -1,7 +1,122 @@
+let numDots = 700; 
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(300, 600);
+  background(242,169,4); 
+  noLoop();
 }
 
+function drawgrass() {
+   // draw the curve for the first huge grass
+   noFill();
+   strokeWeight(4);
+ 
+   let ellipseCenterX = 190, ellipseCenterY = 90;
+   let numCurves = 18; 
+   let lineLength = 100; 
+   let curveAmount = 40; 
+ 
+   for (let i = 0; i < numCurves; i++) {
+    if (random(1) > 0.5) {
+      stroke(209, 79, 127);  
+     } else {
+       stroke(179, 70, 105);      
+     }
+
+     let angle = map(i, 0, numCurves, 0, -PI);  
+     
+     let x1 = ellipseCenterX;
+     let y1 = ellipseCenterY;
+     let x2 = x1 + curveAmount * sin(angle);
+     let y2 = y1 - curveAmount * cos(angle);
+     let x3 = x1 + (lineLength - curveAmount) * cos(angle);
+     let y3 = y1 + (lineLength - curveAmount) * sin(angle);
+     let x4 = x1 + lineLength * cos(angle);
+     let y4 = y1 + lineLength * sin(angle);
+ 
+     bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+   }
+ }
+
 function draw() {
-  background(220);
+  drawgrass();
+
+  stroke(79, 21, 27);
+  line(190, 80, 220, 180)
+
+  // draw the first roots of the huge grass
+  noStroke();
+  fill(183, 90, 125);  
+  ellipse(180, 80, 40, 30);  
+
+  //draw the second roots of the huge grass
+  fill(196, 85, 135);  
+  ellipse(150, 300, 40, 35);
+
+  fill(105, 46, 76);  
+  ellipse(150, 300, 35, 30);
+  
+  fill(252, 105, 155); 
+  ellipse(150, 300, 30, 25);  
+  
+  fill(82, 25, 50); 
+  ellipse(150, 300, 25, 20);
+
+  //draw the third roots of the huge grass
+  fill(229, 82, 139);  
+  ellipse(90, 450, 45, 30);  
+
+  // draw the pink and black dots
+  for (let i = 0; i < numDots; i++) {
+    let x = random(width);
+    let y = random(height);
+    let size = random(6, 9);
+    
+    if (random(1) > 0.5) {
+     fill(206, 72, 121);  
+    } else {
+      fill(0);      
+    }
+    noStroke();
+    ellipse(x, y, size);
+  }
+  drawLeaveCurve()
+}
+
+
+function drawLeaveCurve(){
+  noFill();
+  stroke(0);
+  strokeWeight(5)
+  beginShape();
+  curveVertex(0,140);
+  curveVertex(0,140);
+  curveVertex(80,140);
+  curveVertex(150,155);
+  curveVertex(150,155);
+  endShape();
+  
+  for (let i = 0; i < 7; i++) {
+    noFill();
+    stroke(0);
+    beginShape();
+    curveVertex(30 + i * 19, 140 + i);
+    curveVertex(30 + i * 19, 140 + i * 2);
+    curveVertex(45 + i * 18, 120 + i * 4);
+    curveVertex(75 + i * 19, 100 + i * 6);
+    curveVertex(75 + i * 19, 100 + i * 7);
+    endShape();
+  }
+
+  for (let i = 0; i < 7; i++) {
+    noFill();
+    stroke(0);
+    beginShape();
+    curveVertex(35 + i * 16, 140 + i);
+    curveVertex(35 + i * 16, 140 + i * 2);
+    curveVertex(35 + i * 18, 150 + i * 4);
+    curveVertex(60 + i * 19, 170 + i * 6);
+    curveVertex(35 + i * 19, 180 + i * 7);
+    endShape();
+  }
 }
